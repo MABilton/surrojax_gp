18980495bc99b49da063bd66f2d9bba9a75f8ab8
Matt Bilton <Matt.A.Bilton@gmail.com>
2021-05-20 04:13:31 +0000

    Fixed bugs; code now works when hyperparameters set to 'correct' values without any optimisation.

--- oed_gp.py
+++ oed_gp.py
@@ -1,9 +1,11 @@
 import jax
-import jax.numpy as jnp
+import numpy as jnp
 import jax.scipy.linalg as jlinalg
 
 from math import pi
 
+from scipy.spatial.distance import pdist, cdist
+from scipy.linalg import cholesky, cho_solve
 from scipy.optimize import dual_annealing, shgo
 
 # y_train = (num_samples) - learning a single function!
@@ -19,37 +21,31 @@ class GP_Surrogate:
         self.y_train = y_train
 
         # Vectorise covariance function:
-        cov_fun = jax.vmap(jax.vmap(cov, in_axes=(0,None,None), out_axes=0), in_axes=(None,0,None), out_axes=1)
-        self.cov_fun = lambda x_1, x_2, params : jnp.atleast_1d(cov_fun(x_1, x_2, params).squeeze())
-        
+        self.cov_fun = lambda x_1, x_2, params : cdist(x_1, x_2, lambda x, y, params=params : cov(x, y, params))
+
         # Create function which returns 'noisy' kernel if requested by user:
         if "noise" in constraints:
-            K_fun = lambda x_1, x_2, params : noisy_K(self.cov_fun, x_1, x_2, params)
+            K_fun = lambda x_1, x_2, params : noisy_K(self.cov_fun, x_1, x_2, params, noise_flag=True)
         # Otherwise, add jitter to diagonal of K for numerical stability:
         else:
-            K_fun = lambda x_1, x_2, params : jitter_K(self.cov_fun, x_1, x_2, params)
-            self.K_fun = K_fun
+            K_fun = lambda x_1, x_2, params : noisy_K(self.cov_fun, x_1, x_2, params)
+        self.K_fun = K_fun
 
         # Optimise hyperparameters of covariance function:    
         self.params = opt_hyperparams(x_train, y_train, K_fun, constraints)
 
         # With optimal hyperparameters, compute covariance matrix and inverse covariance matrix:
-        self.cov = K_fun(x_train, x_train, self.params)
-        self.L = jnp.linalg.cholesky(self.cov)
-        self.alpha = jnp.linalg.lstsq((self.L).T, jnp.linalg.lstsq(self.L,y_train)[0])[0]
-
-        # Store vectorised prediction function:
-        self.pred_fun = jax.vmap(pred_fun_temp, in_axes=(None, 0))
+        train_cov = K_fun(x_train, x_train, self.params)
+        self.L = cholesky(train_cov)
+        self.alpha = jnp.linalg.lstsq((self.L).T, jnp.linalg.lstsq(self.L, y_train, rcond=None)[0], rcond=None)[0]
 
     def predict(self, x_new):
-        return self.pred_fun(self, x_new)
-
-def pred_fun_temp(obj, x_new):
-    k = obj.cov_fun(x_new, obj.x_train, obj.params)
-    mean = jnp.dot(k,obj.alpha)
-    v = jnp.linalg.lstsq(obj.L, k)[0]
-    var = obj.cov_fun(x_new, x_new, obj.params) - jnp.dot(v.T, v)
-    return (mean, var)
+        x_new = jnp.atleast_2d(x_new)
+        k = self.cov_fun(self.x_train, x_new, self.params)
+        mean = k.T @ self.alpha 
+        v = jnp.linalg.lstsq(self.L, k, rcond=None)[0]
+        var = jnp.diag(self.cov_fun(x_new, x_new, self.params)) -  jnp.sum(v*v, axis=0, keepdims=True)
+        return (mean, var.T)
 
 # Calls Scipy Optimise function to tune hyperparameters:
 def opt_hyperparams(x_train, y_train, K_fun, constraints):
@@ -66,7 +62,7 @@ def loss(params, x_train, y_train, K_fun, idx_2_key):
     K = K_fun(x_train, x_train, params)
     # Cholesky decomposition:
     L = jnp.linalg.cholesky(K)
-    alpha = jnp.linalg.lstsq(L.T, jnp.linalg.lstsq(L,y_train)[0])[0]
+    alpha = jnp.linalg.lstsq(L.T, jnp.linalg.lstsq(L,y_train,rcond=None)[0],rcond=None)[0]
     n = y_train.shape[0]
     loss  = 0.5*jnp.dot(y_train.T, alpha) + jnp.sum(jnp.log(jnp.diag(L))) + 0.5*n*jnp.log(2*pi)
     return loss.ravel()
@@ -100,15 +96,12 @@ def create_bounds(constraints):
     bounds = list(zip(lb, ub))
     return (bounds, idx_2_key)
 
-def noisy_K(cov_fun, x_1, x_2, params):
-    jitter = 10**(-8)
+def noisy_K(cov_fun, x_1, x_2, params, noise_flag=False):
+    jitter = 10**(-10)
     K = cov_fun(x_1, x_2, params)
-    return K + (params["noise"] + jitter)*jnp.identity(K.shape[0])
-
-def jitter_K(cov_fun, x_1, x_2, params):
-    jitter = 10**(-8)
-    K = cov_fun(x_1, x_2, params)
-    return K + jitter*jnp.identity(K.shape[0])
+    K[jnp.diag_indices_from(K)] += jitter
+    if noise_flag: K[jnp.diag_indices_from(K)] += params["noise"] 
+    return K
 
 #def noisy_K_grad(cov_grad_fun, x_1, x_2, params):
     #cov_grad = cov_grad_fun(x_1, x_2, params)
--- sklearn_example.py
+++ sklearn_example.py
@@ -1,10 +1,12 @@
-import jax.numpy as jnp
+import numpy as jnp
 
 import oed_gp
 
 import random 
 
 from matplotlib import pyplot as plt
+
+from scipy.linalg import cholesky, cho_solve
 
 random.seed(1)
 
@@ -21,24 +23,22 @@ if __name__ == "__main__":
     # Observations:
     y = (f(X).ravel())
     y = y.reshape(len(y),1)
-    x_pred =  jnp.atleast_2d(jnp.linspace(0, 10, 1000)).T
+    x_pred =  jnp.atleast_2d(jnp.linspace(0, 10, 100)).T
     constraints = {"const": {">": 10**(-3)}, "length": {">": 10**(-3)}}
     surrogate = oed_gp.GP_Surrogate(kernel, X, y, constraints)
-    surrogate.params = {"length":10, "const":1}
-    print(jnp.linalg.cholesky(surrogate.K_fun(X, X, surrogate.params)))
-    # y_pred = surrogate.predict(x_pred)
-    
+    y_pred = surrogate.predict(x_pred)
+
     # Plotting: 
-    #fig = plt.figure()
-    #plt.plot(x_pred, f(x_pred), 'r:', label=r'$f(x) = x\,\sin(x)$')
-    #plt.plot(X, y, 'r.', markersize=10, label='Observations')
-    #plt.plot(x_pred, y_pred[0], 'b-', label='Prediction')
-    # plt.fill(jnp.concatenate([x_pred, x_pred[::-1]]),
-    #      jnp.concatenate([y_pred[0] - 1.9600 * y_pred[1],
-    #                     (y_pred[0] + 1.9600 * y_pred[1])[::-1]]),
-    #      alpha=.5, fc='b', ec='None', label='95% confidence interval')
-    # plt.xlabel('$x$')
-    # plt.ylabel('$f(x)$')
-    # plt.ylim(-10, 20)
-    # plt.legend(loc='upper left')
-    # plt.savefig('GP.png', dpi=600)
+    fig = plt.figure()
+    plt.plot(x_pred, f(x_pred), 'r:', label=r'$f(x) = x\,\sin(x)$')
+    plt.plot(X, y, 'r.', markersize=10, label='Observations')
+    plt.plot(x_pred, y_pred[0], 'b-', label='Prediction')
+    plt.fill(jnp.concatenate([x_pred, x_pred[::-1]]),
+         jnp.concatenate([y_pred[0] - 1.9600 * y_pred[1],
+                        (y_pred[0] + 1.9600 * y_pred[1])[::-1]]),
+         alpha=.5, fc='b', ec='None', label='95% confidence interval')
+    plt.xlabel('$x$')
+    plt.ylabel('$f(x)$')
+    plt.ylim(-10, 20)
+    plt.legend(loc='upper left')
+    plt.savefig('GP.png', dpi=600)
